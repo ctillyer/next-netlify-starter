0d09f29a0fa59d52bb9e265f28f7f48b0707f014
ctillyer <84575305+ctillyer@users.noreply.github.com>
2022-07-14 21:17:13 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>ay lmao</title>
+        <title>ayy lmao</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
