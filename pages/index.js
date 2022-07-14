680326ef453bb79e8045332fbc5763109f226b1d
ctillyer <84575305+ctillyer@users.noreply.github.com>
2022-07-14 21:02:40 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>ay lmao</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="pls die krit" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          - chris
         </p>
       </main>
 
