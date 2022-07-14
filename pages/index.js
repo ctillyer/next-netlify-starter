454489a6b1c1015f213d80bcdb231b5c33b10aaf
ctillyer <84575305+ctillyer@users.noreply.github.com>
2022-07-14 20:59:42 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,22 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-
+import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>ay lmao</title>
+        <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="get cancer krit" />
+        <Header title="Welcome to my app!" />
         <p className="description">
-            - from chris
+          Get started by editing <code>pages/index.js</code>
         </p>
       </main>
+
+      <Footer />
     </div>
   )
 }
